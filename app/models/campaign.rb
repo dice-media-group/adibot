b3242823cc54293b75934d4607cc85e0d76b0263
captproton <carl@wdwhub.net>
2017-12-09 20:08:17 -0800

    CHANGED: cleaned up campaign.rb and pages

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,7 +1,7 @@
 class Campaign < ApplicationRecord
 	belongs_to :user
 
-	has_many :tasks
+	has_many :tasks, :dependent => :destroy
 	has_many :merchant_sites, through: :tasks
 
 accepts_nested_attributes_for :tasks, allow_destroy: true,  reject_if: proc { |att| att['merchant_site_id'].blank? }
